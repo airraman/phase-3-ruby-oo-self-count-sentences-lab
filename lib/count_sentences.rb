ccdbb853b3c6bdfadd45f1f924a008a6629fe16c
Raman <ramanmcreates@gmail.com>
2022-11-23 13:31:25 -0500

    commit tree cfe731a7ce9175ce06ab7ae5826d553af485e0cc

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if (self.end_with?(".") == true)
+      true
+    else 
+      false
+    end
   end
 
   def question?
-
+    if (self.end_with?("?") == true)
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if (self.end_with?("!") == true)
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
 
   end
+
+  def report_on_self
+    "Self is: #{self}"
+  end
+
+
 end
+
+r1 = String.new
+
+"hello.".sentence? 
